ad4af1d6100189888e263c2adff8227c9b8b937c
William Lorey <wwlorey@gmail.com>
2019-01-21 08:53:31 -0600

    Fix button aesthetics
    Remove dead code

--- OpenThermApp/screens/HomeScreen.js
+++ OpenThermApp/screens/HomeScreen.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import {
   Alert,
-  Image,
-  Platform,
+  Dimensions,
   ScrollView,
   StyleSheet,
   Text,
-  TouchableOpacity,
+  TouchableHighlight,
   View,
-  Dimensions,
-  Button,
 } from 'react-native';
 import { WebBrowser } from 'expo';
 import { MonoText } from '../components/StyledText';
@@ -18,15 +15,6 @@ class Temperature extends React.Component {
   constructor(props) {
     super(props);
     this.state = { temperature: 69 };
-
-    // Increase the temperature by 1 degree every few seconds
-    /*
-    setInterval(() => (
-      this.setState(previousState => (
-        { temperature: previousState.temperature + 1 }
-      ))
-    ), 2 * 1000);
-    */
   }
 
   render() {
@@ -52,99 +40,37 @@ export default class HomeScreen extends React.Component {
     return (
       <View style={styles.container}>
         <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer} scrollEnabled={false}>
+
           <View style={styles.header}>
             <Temperature/>
           </View>
 
-          <View style={styles.controlBody}>
+         <View style={styles.controlBody}>
+            <View style={styles.controlVerbage}>
 
-            <View style={styles.buttonContainer}>
-              <Button
-                onPress={this._onPressButton}
-                title="Set Temperature"
-                color="#FFFFFF"
-              />
-            </View>
-          
-          </View>
+              <TouchableHighlight onPress={this._onPressButton} underlayColor="white">
+                <View style={styles.button}>
+                  <Text style={styles.buttonText}>Set Temperature</Text>
+                </View>
+              </TouchableHighlight>
 
-          {/*<View style={styles.getStartedContainer}>
-            {this._maybeRenderDevelopmentModeWarning()}
-
-            <Text style={styles.getStartedText}>Get started by opening</Text>
-
-            <View style={[styles.codeHighlightContainer, styles.homeScreenFilename]}>
-              <MonoText style={styles.codeHighlightText}>screens/HomeScreen.js</MonoText>
             </View>
 
-            <Text style={styles.getStartedText}>
-              Change this text and your app will automatically reload.
-            </Text>
+            <View style={styles.controlBackground}></View>
+
           </View>
 
-          <View style={styles.helpContainer}>
-            <TouchableOpacity onPress={this._handleHelpPress} style={styles.helpLink}>
-              <Text style={styles.helpLinkText}>Help, it didn’t automatically reload!</Text>
-            </TouchableOpacity>
-          </View>*/}
         </ScrollView>
 
-        {/*<View style={styles.tabBarInfoContainer}>
-          <Text style={styles.tabBarInfoText}>This is a tab bar. You can edit it in:</Text>
-
-          <View style={[styles.codeHighlightContainer, styles.navigationFilename]}>
-            <MonoText style={styles.codeHighlightText}>navigation/MainTabNavigator.js</MonoText>
-          </View>
-        </View>*/}
       </View>
     );
   }
-
-  _maybeRenderDevelopmentModeWarning() {
-    if (__DEV__) {
-      const learnMoreButton = (
-        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
-          Learn more
-        </Text>
-      );
-
-      return (
-        <Text style={styles.developmentModeText}>
-          Development mode is enabled, your app will be slower but you can use useful development
-          tools. {learnMoreButton}
-        </Text>
-      );
-    } else {
-      return (
-        <Text style={styles.developmentModeText}>
-          You are not in development mode, your app will run at full speed.
-        </Text>
-      );
-    }
-  }
-
-  _handleLearnMorePress = () => {
-    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
-  };
-
-  _handleHelpPress = () => {
-    WebBrowser.openBrowserAsync(
-      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
-    );
-  };
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-  },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
   },
   contentContainer: {
     paddingTop: 30,
@@ -166,8 +92,18 @@ const styles = StyleSheet.create({
     paddingTop: 45,
   },
   controlBody: {
-    // Create an oval top and rectangular sides & bottom
+    flex: 1,
+  },
+  controlVerbage: {
+    position: 'absolute',
     alignItems: 'center',
+    width: Dimensions.get('window').width,
+    zIndex: 1,
+    paddingTop: 90,
+  },
+  controlBackground: {
+    // Create an oval topped shape that fills the page
+    position: 'absolute',
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
     borderRadius: Dimensions.get('window').width / 2,
@@ -176,68 +112,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#CEE2FF',
     transform: [{ scaleX: 2 }],
   },
-  buttonContainer: {
-    marginTop: 100,
+  button: {
+    marginBottom: 30,
+    width: 220,
+    alignItems: 'center',
     backgroundColor: '#003366',
+    borderRadius: 220 / 14,
   },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
+  buttonText: {
+    padding: 20,
+    fontSize: 23,
+    color: 'white',
   },
 });
